d8cfdfd6d333c54cef70d0a60794cf1c857a6fe3
soohyeok8901 <soohyeok8901@gmail.com>
2022-11-12 05:11:25 +0900

    Delete duplicated signatures

--- src/date.ts
+++ src/date.ts
@@ -2,24 +2,6 @@
 export { };
 
 declare global {
-  interface String {
-    /**
-     * 타임존이 Asia/Seoul인 iso 8601 포맷의 datestring을 얻습니다.
-     * */
-    toKorISOString(): string;
-
-    getTime(): number;
-  }
-
-  interface Number {
-    /**
-     * 타임존이 Asia/Seoul인 iso 8601 포맷의 datestring을 얻습니다.
-     * */
-    toKorISOString(): string;
-
-    getTime(): number;
-  }
-
   interface Date {
     /**
      * 타임존이 Asia/Seoul인 iso 8601 포맷의 datestring을 얻습니다.
@@ -28,27 +10,11 @@ declare global {
   }
 }
 
-String.prototype.toKorISOString = function () {
-  return toKorISOString(this as string);
-};
-
-String.prototype.getTime = function () {
-  return new Date(this as string).getTime();
-};
-
-Number.prototype.toKorISOString = function () {
-  return toKorISOString(this as number);
-};
-
-Number.prototype.getTime = function () {
-  return this as number;
-};
-
 Date.prototype.toKorISOString = function () {
   return toKorISOString(this);
 };
 
-const toKorISOString = (value: string | number | Date) => {
+const toKorISOString = (value: Date) => {
   const local = value.getTime() - new Date().getTimezoneOffset() * 60 * 1000;
 
   const offset = (new Date().getTimezoneOffset() + 9 * 60) * 60 * 1000;
--- test/index.ts
+++ test/index.ts
@@ -1,32 +1,19 @@
-import "../src/date"
+import "../src/date";
 
 const test = (name: string, fn: () => void) => {
-  fn()
+  fn();
 
-  console.log(`${name} passed.`)
-}
+  console.log(`${name} passed.`);
+};
 
 const assert = (a: any, b: any) => {
-  if (a !== b) throw Error(`${a} does not equal with ${b}`)
-}
+  if (a !== b) throw Error(`${a} does not equal with ${b}`);
+};
 
 const expected = process.env.TEST_EXPECTED;
 
-test("test string formatter", () => {
-  const result = "2022-01-01 00:00:00".toKorISOString()
-
-  assert(result, expected)
-})
-
 test("test Date formatter", () => {
-  const result = new Date("2022-01-01 00:00:00").toKorISOString()
+  const result = new Date("2022-01-01 00:00:00").toKorISOString();
 
-  assert(result, expected)
-})
-
-test("test number formatter", () => {
-  const result = (1640962800000).toKorISOString()
-
-  assert(result, expected)
-})
-
+  assert(result, expected);
+});
